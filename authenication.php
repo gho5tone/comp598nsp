53b65c429641c0f03cab5c758210862d2ab97e84
gho5tone <itsphilli@gmail.com>
2016-10-11 17:21:48 -0700

    updating all pages with bar and starting to do php

--- authenication.php
+++ authenication.php
@@ -1,4 +1,25 @@
 <!DocType HTML>
 <html lang="en">
+        <head>
+        <meta name="viewport" content="width=device-width, initial-scale=1">
+        <!-- Latest compiled and minified CSS -->
+        <link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/bootstrap/3.3.7/css/bootstrap.min.css" integrity="sha384-BVYiiSIFeK1dGmJRAkycuHAHRg32OmUcww7on3RYdg4Va+PmSTsz/K68vbdEjh4u" crossorigin="anonymous">
+        <meta charset="UTF-8">
+        <link rel="stylesheet" type="text/css" href="/CSS/main.css">
+        <title>the bob </title>
+    </head>
+    <body>
+        <nav class="navbar narbar-default">
+            <div class="container-fluid">
+                <div class="navbar-header">
+                    <a class="navbar-brand" href="#">BobEnterprise</a>
+                </div>
+                <ul class="nav navbar-nav">
+                    <li><a href="item.php"><b>Items</b></a></li>
+                    <li><a href="cart.php"><img id="symbol" src= "http://www.inmotionhosting.com/support/images/stories/icons/ecommerce/64_64_empty-cart-dark.png"></a></li>
+                </ul>
+            </div>
+        </nav>
+    </body>
     Create login/logout
 </html>
--- cart.php
+++ cart.php
@@ -1,5 +1,26 @@
 <!DocType HTML>
 <html lang="en">
+    <head>
+        <meta name="viewport" content="width=device-width, initial-scale=1">
+        <!-- Latest compiled and minified CSS -->
+        <link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/bootstrap/3.3.7/css/bootstrap.min.css" integrity="sha384-BVYiiSIFeK1dGmJRAkycuHAHRg32OmUcww7on3RYdg4Va+PmSTsz/K68vbdEjh4u" crossorigin="anonymous">
+        <meta charset="UTF-8">
+        <link rel="stylesheet" type="text/css" href="/CSS/main.css">
+        <title>Cart </title>
+    </head>
+    <body>
+        <nav class="navbar narbar-default">
+            <div class="container-fluid">
+                <div class="navbar-header">
+                    <a class="navbar-brand" href="index.php">BobEnterprise</a>
+                </div>
+                <ul class="nav navbar-nav">
+                    <li><a href="item.php"><b>Items</b></a></li>
+                    <li><a href="cart.php"><img id="symbol" src= "http://www.inmotionhosting.com/support/images/stories/icons/ecommerce/64_64_empty-cart-dark.png"></a></li>
+                </ul>
+            </div>
+        </nav>
+    </body>
     cart shows items and buy option
     <a href="orderPage.php">click me</a>
 </html>
